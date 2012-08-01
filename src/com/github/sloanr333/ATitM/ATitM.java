03ce668a710bfa2e2e5d5a4c106f678843745a3b
Sloan Reynolds <sloan@lanimals.com>
2012-08-01 14:43:21 -0700

    INITIAL COMMIT

---
+++ src/com/github/sloanr333/ATitM/ATitM.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.sloanr333.ATitM;
+
+/**
+ *
+ * @author Sloan
+ */
+public class ATitM {
+    
+}
